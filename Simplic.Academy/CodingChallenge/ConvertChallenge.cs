d5aedef2777a3badeca6326f0a1c4381630fd087
Benedikt Eggers <be@spiegelburg.net>
2021-01-23 15:40:04 +0100

    Add convert challgen

---
+++ Simplic.Academy/CodingChallenge/ConvertChallenge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace CodingChallenge
+{
+    public class ConvertChallenge
+    {
+        [Fact]
+        public void FixBug_Convert_01()
+        {
+            var value = "24 1";
+
+            var intValue = int.Parse(value);
+
+            Assert.Equal(241, intValue);
+        }
+
+        [Fact]
+        public void FixBug_Convert_02()
+        {
+            var value = "24​1";
+
+            var intValue = int.Parse(value);
+
+            Assert.Equal(241, intValue);
+        }
+
+        [Fact]
+        public void FixBug_Convert_03()
+        {
+            var value = "24.1";
+            var culture = new CultureInfo("de-DE");
+
+            var doubleValue = double.Parse(value, culture);
+
+            Assert.Equal(24.1, doubleValue);
+        }
+    }
+}
